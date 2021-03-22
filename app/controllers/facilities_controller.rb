a4bbbbac0eaa997ea12a201cd4d592d1e7b0894d
masaya-Programming <masaya.31temhr@gmail.com>
2021-03-22 13:08:57 +0900

    検索画面の装飾

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -3,7 +3,13 @@ class FacilitiesController < ApplicationController
   end
 
   def search
+    @category_id = params[:category_id]
+    @prefectures_id = params[:prefectures_id]
     @facility = Facility.search(params[:category_id], params[:prefectures_id])
+    if @facility.length != 0
+      @category_name = @facility[0].category.name
+      @prefectures_name = @facility[0].prefectures.name 
+    end
   end
   
 end
