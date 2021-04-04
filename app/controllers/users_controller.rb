09a6698e19dff6729a568133958c2650d2cd024c
masaya-Programming <masaya.31temhr@gmail.com>
2021-04-04 21:08:47 +0900

    ユーザーページ、マイページへの画面遷移とボタン配置

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:mypage]
+  before_action :set_user, only: [:show]
+
+  def mypage
+    redirect_to user_path(current_user)
+  end
+
+  def show
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "facilities#index"
+
+  resources :users, only: [:show] do
+    member do
+      get 'mypage'
+    end
+  end
+
   resources :facilities, only: [:index, :show] do
     collection do
       get 'search'
