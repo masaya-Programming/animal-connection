54abc841f248d833e2d8cb33f4b12274902a2f87
masaya-Programming <masaya.31temhr@gmail.com>
2021-04-06 01:26:48 +0900

    ページレイアウトの改修

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,12 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:mypage, :edit, :update, :destroy]
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:mypage, :destroy]
+  before_action :set_user, only: [:show, :destroy]
 
   def mypage
     redirect_to user_path(current_user)
   end
 
   def show
-  end
-
-  def edit
-    unless current_user.id == @user.id
-      redirect_to user_path(@user)
-    end
-  end
-
-  def update
-    if current_user.id == @user.id
-      if @user.update(user_params)
-        redirect_to mypage_user_path(current_user)
-      else
-        render :edit
-      end
-    else
-      redirect_to user_path(@user)
-    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
+  
   root to: "facilities#index"
 
-  resources :users, only: [:show, :edit, :update, :destroy] do
+  resources :users, only: [:show, :destroy] do
     member do
       get 'mypage'
     end
