c9ab753060fabe7fba6736f4d9d1869a33866275
masaya-Programming <masaya.31temhr@gmail.com>
2021-07-07 11:42:39 +0900

    SEO対策meta_tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: 'あにまるコネクション',
+      site: 'あにまるコネクション｜動物園・水族館検索サイト',
       title: 'トップページ',
       reverse: true,
       charset: 'utf-8',
-      description: 'あにまるコネクションへようこそ!!日本全国の動物園・水族館を検索できるWebサイトです。多種多様な生物との出会いをつくり、その[思い出]を記録することができます。',
-      keywords: '動物園,水族館,動物,検索',
+      description: '日本全国の動物園・水族館を探すなら「あにまるコネクション」で！ 281件の施設を分かりやすく掲載中。ユーザーが投稿したコメント・画像をもとに、お気に入り施設に巡り合いましょう。いきもの好きの集い場を目指しています。',
+      keywords: 'あにまるコネクション,あにまるこねくしょん,アニマルコネクション,あにコネ,あにこね,アニコネ,動物園検索,水族館検索',
       canonical: request.original_url,
       separator: '|',
       icon: [
@@ -23,9 +23,9 @@ module ApplicationHelper
         local: 'ja-JP',
       },
       twitter: {
-        card: 'summary',
+        card: 'summary_large_image',
         site: '@WanWan_RAMroad',
-        image: asset_path("animal-connection-rogo-meta.png"),
+        image: asset_path("og-image.png"),
         width: 100,
         height: 100
       }
